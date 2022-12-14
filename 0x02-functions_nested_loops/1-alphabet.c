2b941fe86e6f33475d85b0f8b309fc5e38fad440
Ubong Nkanga <datajikwoyi@gmail.com>
2022-12-13 22:28:17 -0800

    Task 1 printing all alpahbet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alpha in lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
