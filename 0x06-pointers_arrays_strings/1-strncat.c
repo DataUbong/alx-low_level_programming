b40381b05ffaf6f8cdc496e3e7d8118d89181d55
Ubong Nkanga <datajikwoyi@gmail.com>
2022-12-22 06:21:40 -0800

    Task 1 coping strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two string using most inputted number from src
+ * @dest: the string to be appended upon
+ * @src: the string to be appended to dest
+ * @n: the maximum number of bytes to copy from src
+ * Return: A pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
