dc7f8321f0dcf6214164beaadc7eb8aa647817c0
Ubong Nkanga <datajikwoyi@gmail.com>
2023-01-05 21:52:34 -0800

    updated

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int actual_sqrt_recursion(int n, int i);
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to calculate square root
@@ -6,10 +7,9 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n == 0 || n == 1)
-		return (n);
-	else if (i * i < n)
-		return (_evaluate(i + 1, n));
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
 }
 /**
  * actual_sqrt_recursion - recurses to find the natural square root
@@ -23,5 +23,5 @@ int actual_sqrt_recursion(int n, int i)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (n, i + 1);
+	return (actual_sqrt_recursion(n, i + 1));
 }
