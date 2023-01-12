f5408fbb8fc9db8619746435cebe075f0dbfe2d8
Ubong Nkanga <datajikwoyi@gmail.com>
2023-01-12 04:07:50 -0800

    Task 0 to create static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * _puts - Functions to insert string
+ * @s: The string to be inserted
+ * Return: Always 0 (Success)
+ */
+void _puts(char *s)
+{
+	puts(s);
+}
